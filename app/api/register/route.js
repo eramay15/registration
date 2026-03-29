d89945700090f238ccd4128a79b72a4839163a15
eramay15 <era.abelgas@gmail.com>
2026-03-29 16:54:01 +0800

    add supabase

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,21 +1,37 @@
-import { connectDB } from '@/lib/db';
+// import { connectDB } from '@/lib/db';
+
+// export async function POST(req) {
+
+//     try {
+//         const { name, password } = await req.json();
+
+//         const db = await connectDB();
+
+//         await db.execute(
+//             'INSERT INTO users (name, password) VALUES (?, ?)',
+//             [name, password]
+//         );
+
+//         return Response.json({ message: 'Saved successfully!' });
+
+//     } catch (error) {
+//         console.log(error);
+//         return Response.json({ message: 'Server error' });
+//     }
+// }
+
+import supabase from "@/lib/supabase"
 
 export async function POST(req) {
+  const { username, password } = await req.json()
 
-    try {
-        const { name, password } = await req.json();
+  const { error } = await supabase
+    .from("users")
+    .insert([{ username, password }])
 
-        const db = await connectDB();
+  if (error) {
+    return Response.json({ error: error.message }, { status: 500 })
+  }
 
-        await db.execute(
-            'INSERT INTO users (name, password) VALUES (?, ?)',
-            [name, password]
-        );
-
-        return Response.json({ message: 'Saved successfully!' });
-
-    } catch (error) {
-        console.log(error);
-        return Response.json({ message: 'Server error' });
-    }
+  return Response.json({ message: "Success" })
 }
